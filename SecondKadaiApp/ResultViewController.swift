a18e9e7632fd6544ccb38844a48699ec0d9368ce
emi <bluming_blue@icloud.com>
2022-03-01 21:00:13 +0900

    エラーの解決方法がわかりません。

---
+++ SecondKadaiApp/ResultViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ResultViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 横田瑛美 on 2022/03/01.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    var x:Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+         let result = x
+          label.text = "こんにちは\(result) さん"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
